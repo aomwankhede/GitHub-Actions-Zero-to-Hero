e9bf49bef294ca3bf493c354d6d4ae52acede9f1
aomwankhede <103637862+aomwankhede@users.noreply.github.com>
2024-12-30 19:27:06 +0530

    Update 2 addition.py

--- src/addition.py
+++ src/addition.py
@@ -1,8 +1,9 @@
 # app.py
 # This is a test commit
+# I will update something here !!!!!!!!
 # Modified Something Here
 def add(a, b):
-    return a + b
+    return a + b - 0 + 0
 
 def test_add():
     assert add(1, 2) == 3
